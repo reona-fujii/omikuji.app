e9de5dbd532c84d1b330b9eaaa046ba9f3c5ebc6
reona-fujii <ya5t4yfxwbcshb908r0s@gmail.com>
2021-01-02 16:04:42 +0900

    edit

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -1,15 +1,20 @@
 package jp.wings.nikkeibp.omikuji
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.main.*
-import java.util.*
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.omikuji.*
 
 class OmikujiActivity : AppCompatActivity() {
+    val omikujiBox = OmikujiBox()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+        setContentView(R.layout.omikuji)
+        omikujiBox.omikujiView = imageView3
 
+
+/*
         val rnd = Random()
         val number = rnd.nextInt(20)
 
@@ -19,5 +24,26 @@ class OmikujiActivity : AppCompatActivity() {
         omikujiShelf[19] = "凶"
         val str = omikujiShelf[number]
         hello_view.text = str
+
+ */
+    }
+    fun onButtonClick(v:View) {
+        omikujiBox.shake()
+//        val translate = TranslateAnimation(0f, 0f, 0f, -200f)
+//        translate.repeatMode = Animation.REVERSE
+//        translate.repeatCount = 5
+//        translate.duration = 100
+//
+//        val rotate = RotateAnimation(0f, -36f, imageView3.width/2f, imageView3.height/2f)
+//        rotate.duration = 200
+//
+//        val set = AnimationSet(true)
+//        set.addAnimation(rotate)
+//        set.addAnimation(translate)
+//
+//
+//
+//        imageView3.startAnimation(set)
+//        imageView3.setImageResource(R.drawable.result1)
     }
 }
--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiBox.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiBox.kt
@@ -1,11 +1,46 @@
 package jp.wings.nikkeibp.omikuji
 
+
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.RotateAnimation
+import android.view.animation.TranslateAnimation
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.omikuji.*
 import java.util.*
 
-class OmikujiBox {
+class OmikujiBox: Animation.AnimationListener {
+    lateinit var omikujiView: ImageView
+    var finish = false //箱から出たか？
     val number : Int //くじ番号
     get() {
         val rnd = Random()
         return rnd.nextInt(20)
+    }
+    fun shake() {
+        val translate = TranslateAnimation(0f, 0f, 0f, -200f)
+        translate.repeatMode = Animation.REVERSE
+        translate.repeatCount = 5
+        translate.duration = 100
+
+        val rotate = RotateAnimation(0f, -36f, omikujiView.width/2f, omikujiView.height/2f)
+        rotate.duration = 200
+
+        val set = AnimationSet(true)
+        set.addAnimation(rotate)
+        set.addAnimation(translate)
+        set.setAnimationListener(this)
+        omikujiView.startAnimation(set)
+        finish = true
+    }
+
+    override fun onAnimationRepeat(animation: Animation?) {
+    }
+
+    override fun onAnimationEnd(animation: Animation?) {
+        omikujiView.setImageResource(R.drawable.omikuji2)
+    }
+
+    override fun onAnimationStart(animation: Animation?) {
     }
 }
