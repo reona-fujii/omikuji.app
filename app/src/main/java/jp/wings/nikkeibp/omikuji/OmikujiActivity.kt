d3ce6bdc7197fe620c1391a0cdaa2fca8586688d
reona-fujii <ya5t4yfxwbcshb908r0s@gmail.com>
2021-01-03 16:33:56 +0900

    edit

--- app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -1,11 +1,22 @@
 package jp.wings.nikkeibp.omikuji
 
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fortune.*
 import kotlinx.android.synthetic.main.omikuji.*
 
 class OmikujiActivity : AppCompatActivity() {
+    //おみくじ棚の配列
+    val omikujiShelf = Array<OmikujiParts>(20)
+    { OmikujiParts(R.drawable.result2, R.string.contents1) }
+
+    //おみくじ番号の保管
+    var omikujiNumber = -1
+
+
     val omikujiBox = OmikujiBox()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,37 +24,42 @@ class OmikujiActivity : AppCompatActivity() {
         setContentView(R.layout.omikuji)
         omikujiBox.omikujiView = imageView3
 
-
-/*
-        val rnd = Random()
-        val number = rnd.nextInt(20)
-
         //おみくじ棚の準備
-        val omikujiShelf = Arrary<String>(20, {"吉"})
-        omikujiShelf[0] = "大吉"
-        omikujiShelf[19] = "凶"
-        val str = omikujiShelf[number]
-        hello_view.text = str
+        omikujiShelf[0].drawID = R.drawable.result1
+        omikujiShelf[0].fortuneID = R.string.contents2
 
- */
+        omikujiShelf[1].drawID = R.drawable.result3
+        omikujiShelf[1].fortuneID = R.string.contents9
+
+        omikujiShelf[2].fortuneID = R.string.contents3
+        omikujiShelf[3].fortuneID = R.string.contents4
+        omikujiShelf[4].fortuneID = R.string.contents5
+        omikujiShelf[5].fortuneID = R.string.contents6
     }
     fun onButtonClick(v:View) {
         omikujiBox.shake()
-//        val translate = TranslateAnimation(0f, 0f, 0f, -200f)
-//        translate.repeatMode = Animation.REVERSE
-//        translate.repeatCount = 5
-//        translate.duration = 100
-//
-//        val rotate = RotateAnimation(0f, -36f, imageView3.width/2f, imageView3.height/2f)
-//        rotate.duration = 200
-//
-//        val set = AnimationSet(true)
-//        set.addAnimation(rotate)
-//        set.addAnimation(translate)
-//
-//
-//
-//        imageView3.startAnimation(set)
-//        imageView3.setImageResource(R.drawable.result1)
+    }
+    fun drawResult() {
+        //おみくじ番号を取得する
+        omikujiNumber = omikujiBox.number
+
+        //おみくじ棚の配列から、omikujiPartsを取得する
+        val op = omikujiShelf[omikujiNumber]
+
+        //レイアウトを運勢表示に変更する
+        setContentView(R.layout.fortune)
+
+        //画像とテキストを変更する
+        imageView.setImageResource(op.drawID)
+        textView3.setText(op.fortuneID)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if(event?.action == MotionEvent.ACTION_DOWN) {
+            if (omikujiNumber < 0 && omikujiBox.finish) {
+                drawResult()
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
